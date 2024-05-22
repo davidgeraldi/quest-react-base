efff3a86aac98db5a92023f47445eb5c0b1600e0
David Geraldi <davidregeraldi@gmail.com>
2024-05-22 12:06:56 -0300

    tentando resolver erro 404 no main.jsx ainda

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -2,11 +2,12 @@ import './button.css'
 
 const showButtonProps = (label, color) => alert(`O texto do botão clicado é "${label}" e a cor do texto é "${color}"`)
 
-const Button = ({label, color}) => <button className='button' style={{color: color}} onClick={() => showButtonProps(label, color)}>{label}</button>
+const Button = ({label = 'Clique aqui', color = 'blue'}) => <button className='button' style={{color: color}} onClick={() => showButtonProps(label, color)}>{label}</button>
 
-Button.defaultProps = {
-    label: 'Clique aqui',
-    color: 'blue'
-}
+
+// Button.defaultProps = {
+//     label: 'Clique aqui',
+//     color: 'blue'
+// }
 
 export default Button
--- src/components/paragraph/paragraph.jsx
+++ src/components/paragraph/paragraph.jsx
@@ -1,10 +1,15 @@
 import './paragraph.css'
 
-const Paragraph = ({label, color}) => <p className="text uppercase" style={{color: color}}>{label}</p>
+// const Paragraph = ({ label, color }) => <p className="text uppercase" style={{ color: color }}>{label}</p>
 
-Paragraph.defaultProps = {
-    color: 'blue',
-    label: 'Texto padrão',
-}
+
+const Paragraph = ({ label = 'Texto padrão', color = 'blue'}) => {
+    return <p className="text uppercase" style={{color: color}}> {label} </p>;
+};
+
+// Paragraph.defaultProps = {
+//     color: 'blue',
+//     label: 'Texto padrão',
+// }
 
 export default Paragraph;
